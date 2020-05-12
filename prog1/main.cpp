36b0855c5178d577dd83df0b2b451efa4bdee70f
shubham-sys <subham.kumar032@gmail.com>
2020-05-12 09:21:51 +0530

    Add Two Number.

---
+++ prog1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a , b, sum = 0;
+    cout << "Hello" <<" Shubham" << endl;
+    cout << "Enter two number " ;
+    cin >>a>>b;
+    sum = a+b;
+    cout << "sum of two number is : ." <<sum<<endl ;
+
+    return 0;
+}
