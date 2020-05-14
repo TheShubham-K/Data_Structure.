2de55867a20bb4d0924ca840ce3f6d51539fe048
shubham-sys <subham.kumar032@gmail.com>
2020-05-14 16:29:56 +0530

    Enum Program

---
+++ enum/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum year{Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+
+int main()
+{
+    int i;
+    for(i = Jan; i<=Dec;i++){
+        printf("%d ", i);
+    }
+    return 0;
+}
